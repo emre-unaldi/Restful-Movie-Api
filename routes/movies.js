d1a095a2bdc01ee7a2f00bfb8f41e0a9d6537b7a
emre-unaldi <emree.unaldi@gmail.com>
2021-12-16 22:24:48 +0300

    ID'ye Göre Kayıt Güncelleme (/api/movies/:movie_id [PUT])

--- routes/movies.js
+++ routes/movies.js
@@ -7,25 +7,23 @@ const Movie = require('../models/Movie');
 
 // Kayıtları listeleme enpointi 
 router.get('/', (req, res) => {
-
   const promise = Movie.find({ });
 
-  promise.then((data) => {
-    res.json(data);
-  }).catch((err) => {
-    res.json(err);
-  });
+    promise.then((data) => {
+      res.json(data);
+    }).catch((err) => {
+      res.json(err);
+    });
 });
-
+ 
 
 // kayıt ekleme endpointi 
 router.post('/', (req, res, next) => {
-
   const movie = new Movie(req.body);  
   const promise = movie.save(); // veritabanını dışarı aktarma ve kontrolü için değişkene atandı
 
     promise.then((data) => { // değişken ile mongoose altındaki promise yapısı kullanıldı.
-      res.json({ status: 1 });
+      res.json(data);
     }).catch((err) => {
       res.json(err);
     });
@@ -40,6 +38,24 @@ router.get('/:movie_id', (req, res, next) => {
       res.json(movie);
     }).catch((err) => {
       res.json(err)
+    })
+});
+
+
+// verilen id ye göre kayıt güncelleme endpointi
+router.put('/:movie_id', (req, res, next) => {
+  const promise = Movie.findByIdAndUpdate(
+    req.params.movie_id,
+    req.body,
+    {
+      new: true  // direk olarak güncel data yı ekrana verir.
+    }
+  );
+
+    promise.then((movie) => {
+      res.json(movie);
+    }).catch((err) => {
+      res.json(err);
     });
 });
 

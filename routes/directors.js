0800c19620c529d87ee218fdf6a145b63f233c17
emre-unaldi <emree.unaldi@gmail.com>
2021-12-17 15:42:10 +0300

    ID'ye göre Director Kayıtlarını Listeleme (/api/directors/:director_id [GET])

--- routes/directors.js
+++ routes/directors.js
@@ -1,4 +1,5 @@
-const express = require('express');
+const mongoose = require('mongoose')
+const express = require('express')
 const router = express.Router();
 
 // Models
@@ -20,6 +21,61 @@ router.post('/', (req, res) => {
 // Directors-Movies join ile birleştirip tüm directorleri listeleme
 router.get('/', (req, res) => {
     const promise = Director.aggregate([ // join işlemi için 
+        {
+            $lookup: {
+                from: 'movies', // nereyle join edileceği
+                localField: '_id', // directors hangi alanla eşleştirilecek
+                foreignField: 'director_id', // movies hangi alanla eşleştirilecek
+                as: 'movies' // gösterilmek için hangi değişkene atanacak
+            }
+        },
+        {
+            $unwind: {
+                path: '$movies', // yukarda oluşturulan datayı kullanmak için almalıyız.
+                preserveNullAndEmptyArrays: true // ilişkisi bulunmayan kayıtlar ıda gösterir.
+            }
+        },
+        {
+            $group: { 
+                _id: {
+                    _id: '$_id',
+                    name: '$name',
+                    surname: '$surname',
+                    bio: '$bio'
+                },
+                movies: { // movies değişkeni altında ne kadar kaydı varsa tek director kaydında göstersin
+                    $push: '$movies'
+                }
+            }
+        },
+        {
+            $project: {
+                _id: '$_id._id',
+                name: '$_id.name',
+                surname: '$_id.surname',
+                bio: '$_id.bio',
+                movies: '$movies'
+
+            }
+        }
+    ]);
+
+        promise.then((data) => {
+            res.json(data);
+        }).catch((err) => {
+            res.json(err);
+        });
+});
+
+
+// verilen director ID'ye göre director-movies kayıtlarını listeleme 
+router.get('/:director_id', (req, res) => {
+    const promise = Director.aggregate([ // join işlemi için 
+        {
+            $match: {
+                '_id': mongoose.Types.ObjectId(req.params.director_id)
+            }
+        },
         {
             $lookup: {
                 from: 'movies', // nereyle join edileceği
